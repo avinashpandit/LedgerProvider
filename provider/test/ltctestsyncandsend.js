47fac5c56d10cbf88e72e81625c60d6c17da5734
avinashpandit <avinash.pandit@gmail.com>
2020-06-03 12:02:36 -0700

    additional test cases

--- provider/test/ltctestsyncandsend.js
+++ provider/test/ltctestsyncandsend.js
@@ -13,32 +13,25 @@ async function main() {
 
   let bridge = await ledgerProvider.getBridge('LTC');
 
-  let pubKey0 = 'Ltub2Z8LKgDDug196j5VBL1fiqPcH6GQXJft6FQfxwp4of5RdM54JhwWAGRQ1MJoChbSMYBG984ou5L9EZftZQRP11fWr5WKLiuzRbr5sv7nS6g';
-  let pubKey1 = 'Ltub2Z8LKgDDug19ABzEdm9cJQjZFfWtCUBEBumQqsdd24bXcCBETriw1SKat2FrGCN9PAcZTaFn6NCkQQTpbs4zfjWWqz3e8sLu1qDZfbSFoGo';
+  //let pubKey = 'Ltub2Z8LKgDDug196j5VBL1fiqPcH6GQXJft6FQfxwp4of5RdM54JhwWAGRQ1MJoChbSMYBG984ou5L9EZftZQRP11fWr5WKLiuzRbr5sv7nS6g';
+  let pubKey = 'Ltub2Z8LKgDDug19ABzEdm9cJQjZFfWtCUBEBumQqsdd24bXcCBETriw1SKat2FrGCN9PAcZTaFn6NCkQQTpbs4zfjWWqz3e8sLu1qDZfbSFoGo';
   let recipientAddress = 'MD7Wh6KJDZfNeCsD7be59ahKdLpN1Nrk5Y';
   //let recipientAddress = 'M8kSYqJdjotrx3evScoHxwfGxa261fy2WV';
   let index = 1;
 
   let options = {
     "currency": "LTC",
-    "amount": "0.38",
+    "amount": "0.1",
     "recipient": [
       recipientAddress
     ],
-    "xpub" : [pubKey1],
-    //"device" : device.path,
+    "xpub" : [pubKey],
     'scheme' : 'segwit',
     'index' : index,
-     //idx : index,
     "length" : 1,
     'feePerByte': '10',
     //"disable-broadcast" : true
   };
-
-    //'self-transaction': true,
-    //'index' : 1,
-    //"length" : 1
-//  };
 
   let addresses  = await ledgerProvider.getAddressForCurrencyList('LTC');
 
@@ -49,13 +42,6 @@ async function main() {
             if (log !== undefined){
               console.log(log);
               let account = log;
-              //options.device =  device.path;
-              //options.xpub = undefined;
-              options.index = index;
-              //account.index = index;
-              //account.starred = false;
-              //account.operationsCount = account.operations ? account.operations.length : 0;
-              //account.freshAddressPath = "49'/2'";
               options.account = account;
             } 
         },
@@ -75,7 +61,19 @@ async function main() {
                 else console.error(String(e.message || e));
               },
               complete: () => {
-                ledgerProvider.openTransport(ledgerProvider);
+                bridge.signAndBroadcastTransaction(options).subscribe({
+                  next: log => {
+                    if (log !== undefined) console.log(log);
+                  },
+                  error: error => {
+                    const e = error instanceof Error ? error : deserializeError(error);
+                    if (process.env.VERBOSE) console.error(e);
+                    else console.error(String(e.message || e));
+                  },
+                  complete: () => {
+                    ledgerProvider.openTransport(ledgerProvider);
+                  }
+                });
               }
             });
         }

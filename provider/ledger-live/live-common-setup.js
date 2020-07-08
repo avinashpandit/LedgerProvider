ba11f790344da78b0c7329409fc4506a9f82e6ad
avinashpandit <avinash.pandit@gmail.com>
2020-07-08 09:02:43 -0700

    many changes to reduce dependecies

--- provider/ledger-live/live-common-setup.js
+++ provider/ledger-live/live-common-setup.js
@@ -37,15 +37,11 @@ var _operators = require("rxjs/operators");
 
 var _hwTransportHttp = _interopRequireDefault(require("@ledgerhq/hw-transport-http"));
 
-var _hwTransportNodeSpeculos = _interopRequireDefault(require("@ledgerhq/hw-transport-node-speculos"));
-
 var _hw = require("@ledgerhq/live-common/lib/hw");
 
 var _promise = require("@ledgerhq/live-common/lib/promise");
 
 var _sanityChecks = require("@ledgerhq/live-common/lib/sanityChecks");
-
-var _speculos = require("@ledgerhq/live-common/lib/load/speculos");
 
 var _nodejs = _interopRequireDefault(require("@ledgerhq/live-common/lib/libcore/platforms/nodejs"));
 
@@ -75,79 +71,11 @@ if (process.env.DEVICE_PROXY_URL) {
   });
 }
 
-const {
-  SPECULOS_APDU_PORT,
-  SPECULOS_BUTTON_PORT,
-  SPECULOS_HOST
-} = process.env;
-
-if (SPECULOS_APDU_PORT) {
-  const req = {
-    apduPort: parseInt(SPECULOS_APDU_PORT, 10)
-  };
-
-  if (SPECULOS_BUTTON_PORT) {
-    req.buttonPort = parseInt(SPECULOS_BUTTON_PORT, 10);
-  }
-
-  if (SPECULOS_HOST) {
-    req.host = SPECULOS_HOST;
-  }
-
-  (0, _hw.registerTransportModule)({
-    id: "tcp",
-    open: () => (0, _promise.retry)(() => _hwTransportNodeSpeculos.default.open(req), {
-      context: "open-tcp-speculos"
-    }),
-    disconnect: () => Promise.resolve()
-  });
-}
 
 const cacheBle = {};
 
 if (!process.env.CI) {
-  let TransportNodeBle;
-
-  const getTransport = () => {
-    if (!TransportNodeBle) {
-      // $FlowFixMe
-      TransportNodeBle = require("@ledgerhq/hw-transport-node-ble").default;
-    }
-
-    return TransportNodeBle;
-  };
-
-  const openBleByQuery = async query => {
-    const m = query.match(/^ble:?(.*)/);
-    if (!m) throw new Error("ble regexp should match");
-    const [, q] = m;
-    if (cacheBle[query]) return cacheBle[query];
-    const t = await (!q ? getTransport().create() : _rxjs.Observable.create(getTransport().listen).pipe((0, _operators.first)(e => (e.device.name || "").toLowerCase().includes(q.toLowerCase()) || e.device.id.toLowerCase() === q.toLowerCase()), (0, _operators.switchMap)(e => TransportNodeBle.open(e.descriptor))).toPromise());
-    cacheBle[query] = t;
-    t.on("disconnect", () => {
-      delete cacheBle[query];
-    });
-    return t;
-  };
-
-  (0, _hw.registerTransportModule)({
-    id: "ble",
-    open: query => {
-      if (query.startsWith("ble")) {
-        return openBleByQuery(query);
-      }
-    },
-    discovery: _rxjs.Observable.create(o => {
-      const s = getTransport().listen(o);
-      return () => s.unsubscribe();
-    }).pipe((0, _operators.map)(e => ({
-      type: e.type,
-      id: "ble:" + e.device.id,
-      name: e.device.name || ""
-    }))),
-    disconnect: query => query.startsWith("ble") ? cacheBle[query] ? getTransport().disconnect(cacheBle[query].id) : Promise.resolve() : null
-  });
-
+  
   const {
     default: TransportNodeHid // eslint-disable-next-line global-require
 
@@ -169,5 +97,4 @@ if (!process.env.CI) {
 
 function closeAllDevices() {
   Object.keys(cacheBle).forEach(_hw.disconnect);
-  (0, _speculos.closeAllSpeculosDevices)();
 }

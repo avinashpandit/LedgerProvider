d84a7562cc4d8457549d2ef84d97ce1dd4bc56e7
avinashpandit <avinash.pandit@gmail.com>
2022-01-07 11:41:00 -0800

    updated

---
+++ provider/test/ltcTxScannerTest.js
@@ -0,0 +1,31 @@
+//import { RippleAPI } from 'ripple-lib';
+import ledgerProvider from '../LedgerProvider';
+import {findCryptoCurrencyByTicker} from '@ledgerhq/live-common/lib/data/cryptocurrencies';
+import {apiForBitcoin} from '../api/Bitcoin';
+
+
+async function main() {
+
+  const address = 'MBRpKQDz493PGubHUxpNiSUUFdbenDQscM';
+
+  const currency = findCryptoCurrencyByTicker('LTC');
+
+  let API = apiForBitcoin(currency);
+
+  let txs = await API.getTransactions(address);
+
+  console.log(`${txs}`);
+  /*
+  const api = new RippleAPI({
+    server: 'wss://s2.ripple.com' // Public rippled server hosted by Ripple, Inc.
+  });
+
+  api.connect().then(() => {
+    api.getTransactions( address, {start: '0BAE93BFEC325C424619204798762C1400E922BC97C4008DC3B2D37FC3406E5D'}).then(transaction => {
+      console.log(`TX : ${transaction}`);
+    });
+  });
+
+  */
+}
+main();
